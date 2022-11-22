43a50b6071ca2f7402a2545700e14a25498549d8
Anna Zakharova <annazakharova0101@gmail.com>
2022-11-23 01:21:22 +0300

    fix tests and linter

--- Makefile
+++ Makefile
@@ -5,10 +5,13 @@ publish:
 	npm publish --dry-run
 
 test: 
-	NODE_OPTIONS=--experimental-vm-modules npx jest
+	NODE_OPTIONS=--experimental-vm-modules npx jest --watch
 
 gendiff:
 	node src/genDiff.js
+
+lint:
+	npx eslint .
 
 	
 
--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -1,10 +1,17 @@
-import genDiff from '../src/genDiff.js'
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import genDiff from '../src/genDiff.js';
 
-const difference = '{\n- follow: false\n  host: hexlet.io\n- proxy: 123.234.53.22\n- timeout: 50\n+ timeout: 20\n+ verbose: true\n}'
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const difference = '{\n- follow: false\n  host: hexlet.io\n- proxy: 123.234.53.22\n- timeout: 50\n+ timeout: 20\n+ verbose: true\n}';
+const filepath1 = path.resolve(__dirname, '../__fixtures__/file1.json');
+const filepath2 = path.resolve(__dirname, '../__fixtures__/file2.json');
 
 test('gendiff', () => {
-    //expect(genDiff('../file1.json', '../file1.json')).toEqual(difference);
-    expect(genDiff('/home/annayellow/frontend-project-46/file1.json', '/home/annayellow/frontend-project-46/file2.json')).toEqual(difference);
-    //expect(genDiff('', '')).toEqual(null);
-    //expect(genDiff('../file1.json', '../file1.js')).toEqual(null);
-})
+  expect(genDiff('./__fixtures__/file1.json', './__fixtures__/file2.json')).toEqual(difference);
+  expect(genDiff(filepath1, filepath2)).toEqual(difference);
+  expect(genDiff('', '')).toEqual(null);
+  expect(genDiff('./__fixtures__/file1.json', './__fixtures__/file1.js')).toEqual(null);
+});
