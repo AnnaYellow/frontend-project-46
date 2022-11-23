c302e278952ca47ecb89b1f302e0c3ff57cb563f
Anna Zakharova <annazakharova0101@gmail.com>
2022-11-23 05:12:27 +0300

    place test result to __ fixtures__

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -1,18 +1,18 @@
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import genDiff from '../src/genDiff.js';
+import result from '../__fixtures__/result.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const difference = '{\n- follow: false\n  host: hexlet.io\n- proxy: 123.234.53.22\n- timeout: 50\n+ timeout: 20\n+ verbose: true\n}';
 const filepath1 = path.resolve(__dirname, '../__fixtures__/file1.json');
 const filepath2 = path.resolve(__dirname, '../__fixtures__/file2.json');
 const filepath3 = path.resolve(__dirname, '../__fixtures__/file1.js');
 
 test('gendiff', () => {
-  expect(genDiff(filepath1, filepath2)).toEqual(difference);
-  expect(genDiff('./__fixtures__/file1.json', './__fixtures__/file2.json')).toEqual(difference);
+  expect(genDiff(filepath1, filepath2)).toEqual(result);
+  expect(genDiff('./__fixtures__/file1.json', './__fixtures__/file2.json')).toEqual(result);
   expect(genDiff('', '')).toEqual(null);
   expect(genDiff(filepath1, filepath3)).toEqual(null);
 });
