4392be9bd7f2c63f8925960b5d11ff8bc46c0094
Anna Zakharova <annazakharova0101@gmail.com>
2022-11-28 20:33:53 +0300

    debug format switcher

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,8 +1,18 @@
+import _ from 'lodash';
+
 import makeStylishDiff from './stylish.js';
 import makePlainDiff from './plain.js';
+import getValue from '../utils.js';
 
 const makeDiff = (data, formatName) => {
-  const { format } = formatName;
+  const getFormat = (declaredName) => {
+    if (!_.isObject(declaredName)) {
+      return declaredName;
+    }
+    return getValue(declaredName, 'format');
+  };
+  const format = getFormat(formatName);
+
   switch (format) {
     case 'stylish': {
       return makeStylishDiff(data);
